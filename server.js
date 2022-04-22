b62e6132e89f7602fab0a125dc377fa22a48e8ff
Jason Cassella <jasoncassella1@gmail.com>
2022-04-22 19:13:29 -0400

    i should include the main api

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.get('/', (request, response) => {
 	response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api/rappers', (request, response) => {
+	response.json(rappers);
+});
+
 app.get('/api/rappers/:rapperName', (request, response) => {
 	const rapName = request.params.rapperName.toLowerCase();
 	console.log(rapName);
